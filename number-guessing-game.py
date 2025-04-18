edfecba272d8be2465576a51a92e956c049ca3e0
Godwin <godwinshibu01@gmail.com>
2025-04-18 22:18:18 +0530

    Day 12

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -2,7 +2,7 @@ import random
 
 
 def make_guess():
-    print("Welcome to the Number Guessing Game! 🔢")
+    print("Welcome to the Number Guessing Game!🔢")
     print("Choose a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
     num = random.randint(1,100)
@@ -12,23 +12,23 @@ def make_guess():
     elif difficulty == 'hard':
         guess_attempt = 5
     else:
-        print("Invalid difficulty selected ❌")
+        print("Invalid difficulty selected❌")
         return
     
     while guess_attempt > 0:
-        print(f"You have {guess_attempt} attempts remaining to guess the number 💀")
-        user_guess = int(input("Make a guess 🤔: "))
+        print(f"You have {guess_attempt} attempts remaining to guess the number💀")
+        user_guess = int(input("Make a guess🤔: "))
 
         if user_guess == num:
-            print("You won 🎆")
+            print("You won🎆")
             return
         elif user_guess < num:
-            print("Too low 🥲")
+            print("Too low🥲")
         else:
-            print("Too high 😅")
+            print("Too high😅")
 
         guess_attempt -= 1
     
-    print("Game Over. You have run out of guesses buddy 😭")
+    print("Game Over. You have run out of guesses buddy😭")
                 
 make_guess()
