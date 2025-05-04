3a3a7a99293fec0ffcd9796da1cf056dd23c32e5
Godwin <godwinshibu01@gmail.com>
2025-05-04 20:49:29 +0530

    updated

--- turtle_gui.py
+++ turtle_gui.py
@@ -1,17 +1,9 @@
-from turtle import Turtle 
+from turtle import Turtle, Screen
 
 jack = Turtle()
 
-
-jack.forward(100)
-jack.left(90)
-jack.forward(100)
-jack.left(90)
-jack.forward(100)
-jack.left(90)
-jack.forward(100)
-jack.left(90)
-jack.forward(100)
-jack.left(90)
-jack.forward(100)
-jack.left(90)
+for steps in range(15):
+    jack.forward(10)
+    jack.pendown()
+    jack.forward(10)
+    jack.penup()
